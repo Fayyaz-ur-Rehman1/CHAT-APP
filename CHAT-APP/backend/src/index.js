cb78125dc1adf97a38b3753e371d09117fa4f48c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-03-10 03:50:25 +0530

    add-message-feature

--- CHAT-APP/backend/src/index.js
+++ CHAT-APP/backend/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookiesParser from "cookie-parser"
+import cookiesParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookiesParser());
 app.use("/api/auth", authRoutes);
+app.use("/messages", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`my server is running at port no. ${PORT}`);
