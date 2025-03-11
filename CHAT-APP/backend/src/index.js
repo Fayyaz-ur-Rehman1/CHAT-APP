2b9e264a2d886dee89e9e7caadb5662100cb08ce
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-03-11 20:23:10 +0530

    Add-Login-Signup-Page

--- CHAT-APP/backend/src/index.js
+++ CHAT-APP/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookiesParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookiesParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
+
 app.use("/api/auth", authRoutes);
 app.use("/messages", messageRoutes)
 
---
+++ CHAT-APP/frontend/src/pages/text.jsx
@@ -0,0 +1,10 @@
+// import {Mail, MessagesSquare, User} from "lucide-react"
+// export default function TestIconshel() {
+//     return (
+//       <div className="p-10 space-x-4">
+//         <User size={32} color="red" />
+//         <Mail size={32} color="blue" />
+//         <MessagesSquare size={32} color="green" />
+//       </div>
+//     );
+//   }
