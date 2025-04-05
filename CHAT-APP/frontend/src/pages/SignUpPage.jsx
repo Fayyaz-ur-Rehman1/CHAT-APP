a74d2f67c774c19a4fc92428c9b6720e0ee23ad8
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-04-05 13:03:58 +0530

    Solve-Some-Bug

--- CHAT-APP/frontend/src/pages/SignUpPage.jsx
+++ CHAT-APP/frontend/src/pages/SignUpPage.jsx
@@ -58,7 +58,7 @@ export const SignUpPage = () => {
                 <span className="label-text font-medium">Full Name</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                   <User className='size-5 text-base-content/40' />
                 </div>
                 <input
@@ -77,7 +77,7 @@ export const SignUpPage = () => {
                 <span className="label-text font-medium">Email</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                   <Mail className="size-5 text-base-content/40" />
                 </div>
                 <input
@@ -96,7 +96,7 @@ export const SignUpPage = () => {
                 <span className="label-text font-medium">Password</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                   <Lock className="size-5 text-base-content/40" />
                 </div>
                 <input
